f6c91a7ddb10dd62411c05ce795c3f9f78f50ff1
jackChen <1272016935@qq.com>
2019-06-05 22:01:38 +0800

    小幅度的修改

--- README.md
+++ README.md
@@ -6,7 +6,20 @@ Give asset image in hexo a absolutely path automatically
 # Usege
 
 ```shell
-npm install hexo-asset-image --save
+package.json dependencies 直接添加例如
+
+{
+  "name": "hexo-site",
+  "version": "0.0.0",
+  "private": true,
+  "hexo": {
+    "version": "3.8.0"
+  },
+  "dependencies": {
+    "hexo": "^3.7.1",
+    "hexo-asset-image": "https://github.com/JackCh3n/hexo-asset-image.git",
+  }
+}
 ```
 
 # Example
@@ -17,6 +30,7 @@ MacGesture2-Publish
 ├── logo.jpg
 └── rules.jpg
 MacGesture2-Publish.md
+建立和文章一样的名字
 ```
 
 Make sure `post_asset_folder: true` in your `_config.yml`.
@@ -26,3 +40,5 @@ Just use `![logo](logo.jpg)` to insert `logo.jpg`.
 # History
 
 2018-04-18: support hexo-abbrlink
+
+2019-06-05:删除多级目录,根据出现的问题进行小幅度的修改
--- index.js
+++ index.js
@@ -10,17 +10,21 @@ hexo.extend.filter.register('after_post_render', function(data){
   var config = hexo.config;
   if(config.post_asset_folder){
     var link = data.permalink;
+    //获取开始位置 第三个 / 的位置 例如 https://nobige.cn/post/20170926-phpUniqueMark/ 前两个刚好是协议/
     var beginPos = getPosition(link, '/', 3) + 1;
-    // In hexo 3.1.1, the permalink of "about" page is like ".../about/index.html".
-    // if not with index.html endpos = link.lastIndexOf('.') + 1 support hexo-abbrlink
-    if(/.*\/index\.html$/.test(link)) {
-      var endPos = link.lastIndexOf('/');
-    }
-    else {
-      var endPos = link.lastIndexOf('.');
-    }
-    link = link.substring(beginPos, endPos) + '/';
 
+    //截取域名后的地址
+    link = link.substring(beginPos);
+
+    /**
+     * 对 about 页面进行特别的处理 index.html
+     * about页面地址：   about/index.html
+     * index.html 长度为10
+     */
+    if (link.substr(-10)="index.html") {
+      link=link.substring(0,link.length-10);
+    }
+    
     var toprocess = ['excerpt', 'more', 'content'];
     for(var i = 0; i < toprocess.length; i++){
       var key = toprocess[i];
@@ -35,21 +39,25 @@ hexo.extend.filter.register('after_post_render', function(data){
       $('img').each(function(){
         if ($(this).attr('src')){
           // For windows style path, we replace '\' to '/'.
+          // 对于windows系统进行反斜杠处理
           var src = $(this).attr('src').replace('\\', '/');
           if(!(/http[s]*.*|\/\/.*/.test(src)
             || /^\s+\//.test(src)
             || /^\s*\/uploads|images\//.test(src))) {
+            /**
+             * 实际使用情况中并没有使用多级目录的习惯，现将其删除
+             */
             // For "about" page, the first part of "src" can't be removed.
             // In addition, to support multi-level local directory.
-            var linkArray = link.split('/').filter(function(elem){
-              return elem != '';
-            });
-            var srcArray = src.split('/').filter(function(elem){
-              return elem != '' && elem != '.';
-            });
-            if(srcArray.length > 1)
-            srcArray.shift();
-            src = srcArray.join('/');
+            // var linkArray = link.split('/').filter(function(elem){
+            //   return elem != '';
+            // });
+            // var srcArray = src.split('/').filter(function(elem){
+            //   return elem != '' && elem != '.';
+            // });
+            // if(srcArray.length > 1)
+            // srcArray.shift();
+            // src = srcArray.join('/');
             $(this).attr('src', config.root + link + src);
             console.info&&console.info("update link as:-->"+config.root + link + src);
           }
